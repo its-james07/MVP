b6b3361a8f6e66edecfa04eea10b31fda888c281
James Karki <karkijames07@gmail.com>
2025-10-11 08:39:47 +0545

    New Changes

---
+++ codeSpace/Script/script.js
@@ -0,0 +1,31 @@
+//Form Modal Toggling
+const closeBtns = document.querySelectorAll('.close-btn');
+const loginModal = document.querySelector('.login-modal');
+const registerModal = document.querySelector('.register-modal');
+const userLogin = document.querySelectorAll('.user-login');
+const createNew = document.querySelector('.create-btn');
+
+closeBtns.forEach((btn) => {
+  btn.onclick = () => {
+    loginModal.style.display = 'none';
+    registerModal.style.display = 'none';
+  };
+});
+
+userLogin.forEach((btn) => {
+  btn.onclick = () => {
+    loginModal.style.display = 'block';
+  };
+});
+
+createNew.onclick = () => {
+  loginModal.style.display = 'none';
+  registerModal.style.display = 'block';
+};
+
+//Form Validation
+function handleLogin(){
+const logForm = document.forms['login-form'];
+
+}
+
